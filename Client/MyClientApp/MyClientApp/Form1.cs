39033cd5caeb1ac09200e3aacf29ec75cc7d0c45
AndreyBabash <babashandrey@gmail.com>
2023-09-09 22:16:21 +0300

    first commit

---
+++ Client/MyClientApp/MyClientApp/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyClientApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            UdpClient udpClient = new UdpClient();
+            try
+            {
+                udpClient.Connect("127.0.0.1", 11000);
+                
+                
+                var sendBytes = GetParams(textBox1.Text,textBox2.Text,textBox3.Text,textBox4.Text);
+                udpClient.Send(sendBytes, sendBytes.Length);
+                await Task.Delay(10);
+                richTextBox1.Text = $"SEND: {sendBytes.Length} bytes";  
+                
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text = ex.Message;
+                
+            }
+
+        }
+
+        static private byte[] GetParams(string p1,string p2,string p3,string ps4)
+        {
+            try
+            {
+                byte[] output = new byte[] { 0 };
+                float param1 = float.Parse(p1);
+                output = BytesAdd(output, getByteOfFloat(param1));
+                float param2 = float.Parse(p2);
+                output = BytesAdd(output, getByteOfFloat(param2));
+                float param3 = float.Parse(p3);
+                output = BytesAdd(output, getByteOfFloat(param3));
+                output = BytesAdd(output, getByteOfString(ps4));
+                return output;
+            }
+            catch (Exception ex)
+            {
+                byte[] output = new byte[] { 1 };
+                output = BytesAdd(output, getByteOfString(ex.Message));
+                return output;
+            }
+        }
+        static byte[] getByteOfFloat(float a)
+        {
+            return BitConverter.GetBytes(a);
+        }
+        static byte[] getByteOfString(string a)
+        {
+            byte[] bytes = new byte[a.Length * 2];
+            for (int i = 0; i < a.Length; i++)
+            {
+                var bytes_temp = BitConverter.GetBytes(a[i]);
+                bytes[i * 2] = bytes_temp[0];
+                bytes[i * 2 + 1] = bytes_temp[1];
+            }
+            return bytes;
+        }
+        static byte[] BytesAdd(byte[] main, byte[] add)
+        {
+            byte[] output = new byte[main.Length + add.Length];
+            for (int i = 0; i < main.Length; i++)
+            {
+                output[i] = main[i];
+            }
+            for (int i = 0; i < add.Length; i++)
+            {
+                output[i + main.Length] = add[i];
+            }
+            return output;
+        }
+    }
+}
+
---
+++ Server/MyServerApp/MyServerApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+
+namespace MyServerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static float getFloatOfByte(byte[] bytes, int start = 0)
+        {
+            return BitConverter.ToSingle(bytes, start);
+        }
+        static string getStringOfByte(byte[] bytes, int start = 0)
+        {
+            string output = "";
+            for (int i = start; i < bytes.Length; i += 2)
+            {
+                char vOut = BitConverter.ToChar(bytes, i);
+                output += vOut;
+            }
+            return output;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] data = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 11000);
+            UdpClient newsock = new UdpClient(ipep);
+            Console.WriteLine("Ожидание данных...");
+            IPEndPoint mysender = new IPEndPoint(IPAddress.Any, 11000);
+
+
+            Task.Run(() =>
+            {
+
+            while (true)
+            {
+                data = newsock.Receive(ref mysender);
+
+                if (data[0] == 0)
+                {
+                    Invoke((MethodInvoker)delegate
+                    {
+                          richTextBox1.Text += $"GET: {data.Length} bytes" + "\n";
+                    });
+
+                    float param1 = getFloatOfByte(data, 1);
+                    float param2 = getFloatOfByte(data, 1 + sizeof(float));
+                    float param3 = getFloatOfByte(data, 1 + 2 * sizeof(float));
+                    string param4 = getStringOfByte(data, 1 + 3 * sizeof(float));
+
+
+                    Invoke((MethodInvoker) delegate
+                    {
+                        richTextBox1.Text += "Par1=" + param1.ToString() + "\n";
+                        richTextBox1.Text += "Par2=" + param2.ToString() + "\n";
+                        richTextBox1.Text += "Par3=" + param3.ToString() + "\n";
+                        richTextBox1.Text += "Par4=" + param4.ToString() + "\n";
+                    });
+
+
+                 }
+                 else if (data[0] == 1)
+                 {
+                        Invoke((MethodInvoker) delegate
+                        {
+                            richTextBox1.Text += $"GET (ERROR): {data.Length} bytes";
+                            string error = getStringOfByte(data, 1);
+                            richTextBox1.Text += error + "\n";
+                        });
+
+
+                    }
+                }
+
+                
+
+             });
+            
+
+        }
+
+
+    }
+}
